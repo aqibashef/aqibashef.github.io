3f2d9424c1ba8619351a97a2ee8a9b4c05f94f6c
Aqib Ashef <aqib1952@gmail.com>
2024-05-18 19:11:52 +0600

    favicon updated and social link to new tab

--- components/social/index.tsx
+++ components/social/index.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import styles from './styles.module.css';
-
 export const Social = () => {
     return (
         <div className="flex gap-x-6">
-            <Link href={"https://twitter.com/aqibashef"}>
+            <Link href={"https://twitter.com/aqibashef"} target="_blank">
                 <Image src={"/twitter-white.svg"} alt="T" height={20} width={20}/>
             </Link>
-            <Link href={"https://www.linkedin.com/in/aqib-ashef-27048753/"}>
+            <Link href={"https://www.linkedin.com/in/aqib-ashef-27048753/"} target="_blank">
                 <Image src={"/linkedin-white.svg"} alt="L" height={20} width={20}/>
             </Link>
         </div>
