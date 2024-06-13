907c642786c320ce62441a87d004ce91fe14254a
Aqib Ashef <aqib1952@gmail.com>
2024-06-13 12:17:57 +0600

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script src='https://www.googletagmanager.com/gtag/js?id=G-91ZF8TTR7M'/>
+      <Script src='js/ganalytics.js'/>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
